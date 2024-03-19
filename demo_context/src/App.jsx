0b0e6d713e1dcb8fb87313e00f159771796c135b
JoeMakuta <makutajosue@gmail.com>
2024-03-19 17:14:10 +0200

    Handle darkmode in app

--- demo_context/src/App.jsx
+++ demo_context/src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <>
+    <main className={theme == "light" ? "light-mode" : "dark-mode"}>
       <h1>DEMO : REACT_CONTEXT</h1>
-    </>
+      <button
+        onClick={() => {
+          setTheme("light");
+        }}
+      >
+        Light
+      </button>
+      <button
+        onClick={() => {
+          setTheme("dark");
+        }}
+      >
+        Dark
+      </button>
+    </main>
   );
 }
 
